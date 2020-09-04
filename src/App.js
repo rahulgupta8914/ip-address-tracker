8f27c06ea8fd6ac5d82c860d7580c8b928c1193b
Rahul Gupta <gupta8914@gmail.com>
2020-09-04 23:57:43 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="search-area">
+        <h1>
+        IP Address Tracker
+        </h1>
+        <input placeholder="Search for any IP address or domain" />
+        <button title="search"> Search</button>
+        </div>
+            <div className="card-area">
+            card area 
+          </div>
+          <div className="mapview">
+            Map Area
+          </div>
       </header>
-    </div>
   );
 }
 
